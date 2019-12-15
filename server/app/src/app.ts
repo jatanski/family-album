e39d166ecccf9744d40ad12d616cafa762564c34
deBroglie <tanski.gustaw@gmail.com>
2019-12-15 14:19:47 +0100

    cors bug fix

--- server/app/src/app.ts
+++ server/app/src/app.ts
@@ -1,9 +1,10 @@
 import express, { json } from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+const dotenvConfigResult = dotenv.config();
 import EnvVarError from "../lib/errors/EnvVarError";
 import { LoginAPI } from "./enpoints/login/LoginAPI";
-const dotenvConfigResult = dotenv.config();
 if (dotenvConfigResult.error) throw dotenvConfigResult.error;
 
 console.clear();
@@ -20,6 +21,7 @@ const PORT: string = process.env.PORT ?? "3000";
 const app = express();
 
 app.use(json());
+app.use(cors());
 app.use("/login", new LoginAPI().router);
 
 app.listen(PORT, () => {
