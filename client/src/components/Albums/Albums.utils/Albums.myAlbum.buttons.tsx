30643b30cde49a3d2780ec444baa721355599ec6
Kuba <ja.tanski@gmail.com>
2019-12-23 21:08:01 +0100

    add posibility go to carusel from albums

--- client/src/components/Albums/Albums.utils/Albums.myAlbum.buttons.tsx
+++ client/src/components/Albums/Albums.utils/Albums.myAlbum.buttons.tsx
@@ -3,19 +3,22 @@ import { MDBBtn } from 'mdbreact';
 import { AlbumTypeAndSetAlbum } from '../Album.types';
 import { Link } from 'react-router-dom';
 
-const AlbumButtons: FC<AlbumTypeAndSetAlbum> = ({ setAlbum, _id }) => (
-	<div className="albums__wrap--el--buttons">
-		<Link to="carousel">
-			<MDBBtn onClick={setAlbum} id={_id} color="red" size="sm">
-				Obejrzyj
-			</MDBBtn>
-		</Link>
-		<Link to="add">
-			<MDBBtn onClick={setAlbum} id={_id} color="indigo" size="sm">
-				Dodaj zdjęcia
-			</MDBBtn>
-		</Link>
-	</div>
-);
+const AlbumButtons: FC<AlbumTypeAndSetAlbum> = ({ setAlbum, _id }) => {
+	const linkToCarouselWithAlbumId = `carousel/${_id}`;
+	return (
+		<div className="albums__wrap--el--buttons">
+			<Link to={linkToCarouselWithAlbumId}>
+				<MDBBtn onClick={setAlbum} id={_id} color="red" size="sm">
+					Obejrzyj
+				</MDBBtn>
+			</Link>
+			<Link to="add">
+				<MDBBtn onClick={setAlbum} id={_id} color="indigo" size="sm">
+					Dodaj zdjęcia
+				</MDBBtn>
+			</Link>
+		</div>
+	);
+};
 
 export default AlbumButtons;
